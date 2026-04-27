0a64d4d51868fe0a6d7d4de53cf36f763bb22c8c
levdkashirin-wq <levd.kashirin@gmail.com>
2026-04-27 13:50:49 +0300

    Update TodoApiApplicationTests.kt

--- src/test/kotlin/com/example/todo_api/TodoApiApplicationTests.kt
+++ src/test/kotlin/com/example/todo_api/TodoApiApplicationTests.kt
@@ -1,13 +1,1 @@
-package com.example.todo_api
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class TodoApiApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
