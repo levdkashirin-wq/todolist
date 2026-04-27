aacb8638c1f38254e439f57c19b83362465304ec
levdkashirin-wq <levd.kashirin@gmail.com>
2026-04-27 16:29:13 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,19 +25,17 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:postgresql:42.6.0")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.jdbi:jdbi3-core:3.47.0")
 	implementation("org.jdbi:jdbi3-sqlobject:3.47.0")
 	implementation("org.jdbi:jdbi3-spring5:3.47.0")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
-	runtimeOnly("org.postgresql:postgresql")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	runtimeOnly("com.h2database:h2")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	testRuntimeOnly("com.h2database:h2")
+
 }
 
 kotlin {
